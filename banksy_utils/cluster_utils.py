bdc030fa4c4cb931dc86723972bd34584b25b4a6
Nathalie Nataren <nj.nataren@gmail.com>
2026-04-17 16:47:36 +0930

    Updates to QC script and initialisation of clustering script -Added several comments to `00_QC_testing_xenium_spatial.ipynb` and removed extraneous code -Initial commit for the `xenium_clustering.ipynb` script that will be used to cluster xenium spatial samples on the HPC -Modification to `xenium_qc_and_clustering_CK_skin_res.ipynb` which was used to test clustering of corrected adata object that where converted directly from raw data to adata (removing the intermediate seurat + SCTransform)

--- banksy_utils/cluster_utils.py
+++ banksy_utils/cluster_utils.py
@@ -123,7 +123,7 @@ def create_spatial_nonspatial_adata(results_df: pd.DataFrame,
     Creates spatial and nonspatial anndata object from results_df
     """
     #params_name = f"scaled_gaussian_pc{pca_dims[0]:2d}_nc{lambda_list[0]:0.2f}_r{resolutions[0]:0.2f}"
-    params_name = f"{weights_scheme}_pc{pca_dims[0]:2d}_nc{lambda_list[0]:0.2f}_r{resolutions[0]:0.2f}"
+    params_name = f"{weights_scheme}_pc{pca_dims[0]:2d}_nc{lambda_list[0]:0.2f}_r{resolutions[0]:0.2f}" # removed the hard coding for the weight_scheme
     print("Using spatial params:", params_name)
     adata_spatial = results_df.loc[params_name, "adata"]
     label_name = f"labels_{params_name}"
